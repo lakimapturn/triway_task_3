271c8912428e3a975c4952be4e5225eac1f7a6b3
lakimapturn <venomizer11@gmail.com>
2021-07-08 11:38:49 +0400

    added server.js

--- server.js
+++ server.js
@@ -8,9 +8,9 @@ function requireHTTPS(req, res, next) {
 const express = require('express');
 const app = express();
 app.use(requireHTTPS);
-app.use(express.static('./dist/task3'));
+app.use(express.static('./dist/task3.json'));
 app.get('/*', function(req, res) {
-    res.sendFile('index.html', {root: 'dist/task3/'}
+    res.sendFile('index.html', {root: 'dist/task3.json/'}
   );
   });
 app.listen(process.env.PORT || 8080);
